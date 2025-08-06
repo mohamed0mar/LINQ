82833e11f04ae460cdad1b34c44597e97a63bc01
Mohamed Mahmoud <Mohamed_20180534@fci.helwan.edu.eg>
2025-08-06 15:03:33 +0300

    -LINQ

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -9,7 +9,9 @@
             Console.WriteLine("All Cars:");
             CarRepository.PrintCars(cars);
 
-           
+            Console.WriteLine("\n\nCars manufactured after 2021:");
+            var newCars = cars.Where(c => c.ManufactureYear > 2021);
+            CarRepository.PrintCars(newCars);
         }
     }
 }
